6d7e48ef96852476a7cf0eb87590f6cae5a568fd
ClementLegouest <clement.legouest@gmail.com>
2020-03-12 15:37:50 +0100

    initialized structure of the project

---
+++ src/TodoDone.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TodoDone.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/TodoDone.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TodoDone.Core
+{
+    public class Class1
+    {
+    }
+}
