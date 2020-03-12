6cae4c5f44f2e33ee3daad60d6cf726b704dd2d9
ClementLegouest <clement.legouest@gmail.com>
2020-03-12 15:47:24 +0100

    task class creation

---
+++ src/TodoDone.Core/Task.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TodoDone.Core
+{
+    public class Task
+    {
+        private DateTime creationDate;
+        private string task;
+
+        public Task(string task)
+        {
+            this.creationDate = DateTime.Now;
+            this.task = task;
+        }
+        
+        public DateTime getCreationDate()
+        {
+            return this.creationDate;
+        }
+
+        public string getTask()
+        {
+            return task;
+        }
+    }
+}
